c76603cfc384952b9c8aa23adeb9b23e90bd36a0
Daniel Niedergesäß <daniel.niedergesaess@gmail.com>
2016-07-28 23:59:00 +0200

    added license

--- src/app/code/community/Smartdevs/Indexer/Helper/Data.php
+++ src/app/code/community/Smartdevs/Indexer/Helper/Data.php
@@ -1,12 +1,21 @@
 <?php
 
 /**
- * Smartdevs indexer Helper
+ * SmartDevs Indexer Performance fix extension
  *
- * @category    Smartdevs
- * @package     Smartdevs_Indexer
- * @author      Daniel Niedergesäß <daniel.niedergesaess@gmail.com>
+ * NOTICE OF LICENSE
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ * @category   SmartDevs
+ * @package    Smartdevs_Indexer
+ * @copyright  Copyright (c) 2016 Smart-Devs UG (haftungsbeschränkt) (http://www.smart-devs.rocks)
+ * @license    http://www.wtfpl.net/  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * @author     Daniel Niedergesäß <dn@smart-devs.rocks>
  */
+
 class Smartdevs_Indexer_Helper_Data extends Mage_Core_Helper_Abstract
 {
 
--- src/app/code/community/Smartdevs/Indexer/Model/Resource/Indexer/Stock.php
+++ src/app/code/community/Smartdevs/Indexer/Model/Resource/Indexer/Stock.php
@@ -1,11 +1,18 @@
 <?php
-
 /**
- * CatalogInventory Stock Status Indexer Resource Model
+ * SmartDevs Indexer Performance fix extension
  *
- * @category    Smartdevs
- * @package     Smartdevs_Indexer
- * @author      Daniel Niedergesäß <daniel.niedergesaess@gmail.com>
+ * NOTICE OF LICENSE
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ * @category   SmartDevs
+ * @package    Smartdevs_Indexer
+ * @copyright  Copyright (c) 2016 Smart-Devs UG (haftungsbeschränkt) (http://www.smart-devs.rocks)
+ * @license    http://www.wtfpl.net/  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * @author     Daniel Niedergesäß <dn@smart-devs.rocks>
  */
 class Smartdevs_Indexer_Model_Resource_Indexer_Stock extends Mage_CatalogInventory_Model_Resource_Indexer_Stock
 {
